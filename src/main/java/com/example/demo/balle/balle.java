2df2c0335b2233129f25afb18bb9ddca1a451969
Aniket <aniketmahajan20@gmail.com>
2025-01-13 12:11:57 -0800

    first commit

---
+++ src/main/java/com/example/demo/balle/balle.java
@@ -0,0 +1,5 @@
+package com.example.demo.balle;
+
+public class balle {
+
+}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,17 @@
+package com.example.demo.student;
+
+public class StudentController {
+
+}
+
+// @GetMapping
+// 	public List<Student> hello() {
+// 		return List.of(
+// 			new Student(1L, 
+// 				"Aniket", 
+// 				21,
+// 				LocalDate.of(2000, Month.JANUARY, 5),
+// 				"aniketmahajan@gmail.com"
+// 				)
+// 			);
+// 	}
